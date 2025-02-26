9090142e901ab9a4561f5c4ebbc19081a23c44f1
Sid Lalgowdar <sidlalgowdar@Sids-Air.eduroam-wless.du.edu>
2025-02-25 22:56:50 -0700

    idk if it works lmao

--- file_explorer.py
+++ file_explorer.py
@@ -3,6 +3,7 @@ import pygame, sys
 from pygame.locals import *
 import os
 import shutil
+import json
 
 pygame.init()
 
@@ -19,44 +20,28 @@ tag_dict = {}
 cmds = ["!delete-file", "!tag-add", "!tag-remove", "!rename", "!refresh", "!tag-remove-all", "!tag-show"]
 vars_ = []
 
-def get_dirs_and_files(root, tag, tag_dict):
+def get_files(root):
     for en in os.scandir(root):
-        try:
-            if en.is_file(follow_symlinks=False):
-                if en.name.lower() != ".ds_store":
-                    tag_dict[tag].append(en.name)
-                    yield en.name
-            elif en.is_dir(follow_symlinks=False):
-                tag = en.name
-                tag_dict[tag] = []
-                yield en.name
-                yield from get_dirs_and_files(en.path, tag, tag_dict)
-        except:
-            pass
+        if en.name.lower() not in [".ds_store", "all_files.json"] and en.is_file(follow_symlinks=False):
+            yield en.name
 
 def initial_setup(root, cmds, vars_):
-	tagged = os.path.join(root, "Documents/My Tagged Files")
-	if os.path.exists(tagged) == False:
-		os.mkdir(tagged)
-		image_tagged = os.path.join(tagged, "#img")
-		os.mkdir(image_tagged)
-		files_tagged = os.path.join(tagged, "#file")
-		os.mkdir(files_tagged)
-		misc_tagged = os.path.join(tagged, "#misc")
-		os.mkdir(misc_tagged)
-		no_tag = os.path.join(tagged, "#untagged")
-		os.mkdir(no_tag)
-	
-	tags = [path for path in get_dirs_and_files(tagged, None, tag_dict)]
-	tags.extend(cmds)
-	tags.extend(vars_)
-	curr_path = tagged
-	return tags, curr_path
+    tagged = os.path.join(root, "Documents/My Tagged Files")
+    if os.path.exists(tagged) == False:
+        os.mkdir(tagged)
+        with open(os.path.join(tagged, 'all_files.json'), 'w') as file:
+            json.dump({"#untagged": [], "#img": [], "#file": [], "#misc": []}, file, indent=4)
+    with open(os.path.join(tagged, "all_files.json"), "r") as file:
+        tag_dict = json.load(file)
+    tags = [path for path in get_files(tagged)]
+    tags.extend([t for t in tag_dict])
+    tags.extend(cmds)
+    tags.extend(vars_)
+    curr_path = tagged
+    return tags, curr_path, tag_dict
 
 print("Initializing...")
-tags, curr_path = initial_setup(os.path.expanduser("~"), cmds, vars_)
-
-default_path = os.path.join(curr_path, "#untagged")
+tags, curr_path, tag_dict = initial_setup(os.path.expanduser("~"), cmds, vars_)
 
 class TrieNode:
 	def __init__(self):
@@ -132,75 +117,94 @@ if tags:
 		if t:
 			tab_tree.insert(t)
 
-font_ = pygame.font.SysFont("Courier", 10)
+font_ = pygame.font.SysFont("Courier", 20)
 
-def parse_command(command, tag_dict, tags, tab_tree):
-    root_dir = os.path.expanduser("~")
-    command_list = command.strip().split(">")
+def parse_command(command, tag_dict, tags, tab_tree, root_dir):
+    command_list = command.strip().split(" > ")
     com = command_list[0].strip()
     if com == "!delete-file":
+        command_list[1] = command_list[1].strip()
         if "#" in command_list[1]:
-            return "Invalid file name", tags, tag_dict, tab_tree
+            return ["Invalid file name"], tags, tag_dict, tab_tree
         try:
             for t in tag_dict:
                 if command_list[1] in tag_dict[t]:
-                    os.remove(os.path.join(root_dir, t, command_list[1]))
                     tag_dict[t].remove(command_list[1])
+            os.remove(os.path.join(root_dir, command_list[1]))
             tags.remove(command_list[1])
             tab_tree.remove(command_list[1])
-            return f"Removed {command_list[1]}", tags, tag_dict, tab_tree
+            return [f"Removed {command_list[1]}"], tags, tag_dict, tab_tree
         except OSError:
-            return "File not found", tags, tag_dict, tab_tree
+            return ["File not found"], tags, tag_dict, tab_tree
         except:
-            return "Error deleting file", tags, tag_dict, tab_tree 
+            return ["Error deleting file"], tags, tag_dict, tab_tree 
 
     elif com == "!rename":
-        if "#" in command_list[1] or "&&" in command_list[1] or "#" in command_list[2] or "&&" in command_list[2]:
-            return "Invalid file name", tags, tag_dict, tab_tree
+        command_list[1], command_list[2] = command_list[1].strip(), command_list[2].strip()
+        if "#" in command_list[1] or "&" in command_list[1] or "#" in command_list[2] or "&" in command_list[2] or ">" in command_list[1] or ">" in command_list[2]:
+            return ["Invalid file name"], tags, tag_dict, tab_tree
         try:
             for t in tag_dict:
-                if command_list[1] in set(tag_dict[t]):
-                    os.rename(os.path.join(root_dir, t, command_list[1]), os.path.join(root_dir, t, command_list[2]))
+                if command_list[1] in tag_dict[t]:
                     tag_dict[t][tag_dict[t].index(command_list[1])] = command_list[2]
+            os.rename(os.path.join(root_dir, command_list[1]), os.path.join(root_dir, command_list[2]))
             tags[tags.index(command_list[1])] = command_list[2]
             tab_tree.remove(command_list[1])
             tab_tree.insert(command_list[2])
-            return f"Renamed {command_list[1]} to {command_list[2]}", tags, tag_dict, tab_tree
+            return [f"Renamed {command_list[1]} to {command_list[2]}"], tags, tag_dict, tab_tree
         except OSError:
-            return "File not found", tags, tag_dict, tab_tree
+            return ["File not found"], tags, tag_dict, tab_tree
         except:
-            return "Error renaming file", tags, tag_dict, tab_tree
+            return ["Error renaming file"], tags, tag_dict, tab_tree
+    
+    elif com == "!tag-add":
+        command_list[1], command_list[2] = command_list[1].strip(), command_list[2].strip()
+        if "#" in command_list[1]:
+            return ["Invalid file name"], tags, tag_dict, tab_tree
+        list_of_tags = command_list[2].split(" & ")
+
+        for l in list_of_tags:
+            if l[0] == "#" and l != "#untagged":
+                l = l.strip()
+                if l not in tag_dict:
+                    tag_dict[l] = []
+                    tab_tree.insert(l)
+                tag_dict[l].append(command_list[1])
+        return ["Tags added"], tags, tag_dict, tab_tree
 
     else:
-        if "&&" not in com and "#" not in com:
+        if "&" not in com and "#" not in com and ">" not in com:
             try:
                 for t in tag_dict:
                     if com in tag_dict[t]:
-                        os.system(f"cd '{os.path.join(os.path.expanduser('~'), 'Documents/My Tagged Files')}' && open '{os.path.join(t, com.strip())}'")
-                        return f"Opened {com.strip()}", tags, tag_dict, tab_tree
-                return "Couldn't open file", tags, tag_dict, tab_tree
+                        os.system(f"cd '{root_dir}' && open '{com.strip()}'")
+                        return [f"Opened {com.strip()}"], tags, tag_dict, tab_tree
+                return ["Couldn't open file"], tags, tag_dict, tab_tree
             except:
-                return "Error opening file", tags, tag_dict, tab_tree
+                return ["Error opening file"], tags, tag_dict, tab_tree
 
-        list_of_tags_or_file = com.strip().split("&&")
-        all_files = set()
-        for ltf in list_of_tags_or_file:
+        list_of_tags = com.strip().split(" & ")
+        all_files = set(tag_dict[list_of_tags[0]])
+        for ltf in list_of_tags:
             ltf = ltf.strip()
-            for f in tag_dict[ltf]:
-                if f not in all_files:
-                    all_files.add(f)
-        return "\n".join(list(all_files)), tags, tag_dict, tab_tree
+            all_files = all_files & set(tag_dict[ltf])
+        return list(all_files), tags, tag_dict, tab_tree
 
-feed_back = ""
+feed_back = [""]
+tokens = [] 
+token = ""
+vs = 0
+ve = 15
 
 while True:
     screen.fill((0,0,0))
     entry.render(screen)
-    feed_surf = font_.render(feed_back, True, (255,255,255))
+    feed_surf = font_.render("\n".join(feed_back[vs:ve]), True, (255,255,255))
     screen.blit(feed_surf, (50, 80))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print(tag_dict)
+            with open(os.path.join(curr_path, "all_files.json"), "w") as file:
+                json.dump(tag_dict, file, indent=4)
             sys.exit(0)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
@@ -212,14 +216,14 @@ while True:
             file_path = event.file
             file_dir = os.path.dirname(file_path)
             file_name = os.path.basename(file_path)
-            if "#" in file_name or "&&" in file_name:
-                feed_back = "Invalid file name"
+            if "#" in file_name or "&" in file_name or ">" in file_name:
+                feed_back = ["Invalid file name"]
             else:
-                if os.path.exists(os.path.join(default_path, file_name)) == False:
+                if os.path.exists(os.path.join(curr_path, file_name)) == False:
                     new_file_name = file_name.replace(" ", "_")
-                    shutil.move(file_path, default_path)
-                    os.rename(os.path.join(default_path, file_name), os.path.join(default_path, new_file_name))
-                    feed_back = f"{new_file_name} added to #untagged"
+                    shutil.move(file_path, curr_path)
+                    os.rename(os.path.join(curr_path, file_name), os.path.join(curr_path, new_file_name))
+                    feed_back = [f"{new_file_name} added to #untagged"]
                     tag_dict["#untagged"].append(new_file_name)
                     tags.append(new_file_name)
                     tab_tree.insert(new_file_name)
@@ -227,30 +231,56 @@ while True:
             if entry.is_active() == True and entry.is_hidden() == False:
                 if event.key == pygame.K_BACKSPACE:
                     entry.remove_behind_cursor()
-                    suggestions = tab_tree.find_prefix(entry.get_text())
+                    if token:
+                        token = token[:-1]
+                    else:
+                        token = tokens[-1] if tokens else ""
+                        tokens = tokens[:-1]
+                    suggestions = tab_tree.find_prefix(token)
                     suggestions.sort()
                     suggestions = suggestions[:10]
                     curr_sug = 0
                 elif event.key == pygame.K_RETURN:
                     command = entry.get_text()
-                    feed_back, tags, tag_dict, tab_tree = parse_command(command, tag_dict, tags, tab_tree)
+                    feed_back, tags, tag_dict, tab_tree = parse_command(command, tag_dict, tags, tab_tree, curr_path)
                     entry.set_text("")
+                    entry.set_inactive()
+                    token = ""
+                    tokens = []
                 elif event.key == pygame.K_LEFT:
                     entry.move_cursorx(-1)
                 elif event.key == pygame.K_RIGHT:
                     entry.move_cursorx(1)
+                
                 elif event.key == pygame.K_TAB:
                     if suggestions:
-                        entry.set_text(suggestions[curr_sug])
+                        token = suggestions[curr_sug]
+                        entry.set_text("".join(tokens[:-1]) + token)
                         curr_sug = (curr_sug+1)%len(suggestions)
                     else:
                         curr_sug = 0
                 elif event.unicode:
                     entry.append_at_cursor(event.unicode)
-                    suggestions = tab_tree.find_prefix(entry.get_text())
-                    suggestions.sort()
-                    suggestions = suggestions[:10]
+                    if event.unicode == ">" or event.unicode == " " or event.unicode == "&":
+                        tokens.append(token)
+                        tokens.append(event.unicode)
+                        tokens.append("")
+                        suggestions = []
+                        token = ""
+                    else:
+                        token += event.unicode
+                        suggestions = tab_tree.find_prefix(token)
+                        suggestions.sort()
+                        suggestions = suggestions[:10]
                     curr_sug = 0
+            elif event.key == pygame.K_UP:
+                if vs > 0:
+                    vs -= 1
+                    ve -= 1
+            elif event.key == pygame.K_DOWN:
+                if ve < len(feed_back):
+                    vs += 1
+                    ve += 1
 
     pygame.display.update()
     clock.tick(30)
